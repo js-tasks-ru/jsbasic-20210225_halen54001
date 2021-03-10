835d07617f0aa98bad6cdcb6439502b2ef06ec72
destors <halen54001@gmail.com>
2021-03-10 14:06:14 +0200

    module3_task1_temp2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,5 +1,6 @@
 function sumSalary(salaries) {
   let sum = 0;
+  // не могу разобраться как добавить проверку salaries[key] == "number" на `NaN`, `Infinity`, `-Infinity`
   for (let key in salaries) {
     if (typeof salaries[key] == "number") {
       sum += salaries[key];
