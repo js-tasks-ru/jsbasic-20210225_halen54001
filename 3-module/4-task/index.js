834360740b30cabf3c93ec974f20b283336e5312
destors <halen54001@gmail.com>
2021-03-19 13:58:50 +0200

    3-module_4-task_ready

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let usersAgefilter = users.filter(function (person) {
+    if (person.age <= age) {
+      return person;
+    }
+  });
+  let mappedUsers = usersAgefilter.map(
+    (person) => `${person.name}, ${person.balance}`
+  );
+  return mappedUsers.join("\n");
 }
