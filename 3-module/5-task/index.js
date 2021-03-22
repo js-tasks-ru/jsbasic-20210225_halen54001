a5077b243d3e1458ac693542a412a025bb0f1b1a
destors <halen54001@gmail.com>
2021-03-22 17:12:08 +0200

    3-module_5task_update

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,9 +1,5 @@
 function getMinMax(str) {
   let inputArray = str.split(/,| /);
   let onlyNumbers = inputArray.filter(Number);
-  let result = {
-    min: Math.min(...onlyNumbers),
-    max: Math.max(...onlyNumbers),
-  };
-  return result;
+  return { min: Math.min(...onlyNumbers), max: Math.max(...onlyNumbers) };
 }
