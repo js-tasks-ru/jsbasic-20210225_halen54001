b648edaa876dcce1c345aee81533949164303dc1
destors <halen54001@gmail.com>
2021-03-23 21:02:28 +0200

    4-module/1-task_ready

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(array) {
+  let mappedUserNames = array.map(
+    (person) => `${person.firstName} ${person.lastName}`
+  );
+  let list = document.createElement("ul");
+  for (let i = 0; i < mappedUserNames.length; i++) {
+    let item = document.createElement("li");
+    item.appendChild(document.createTextNode(mappedUserNames.join(" ")));
+    list.appendChild(item);
+  }
+  return list;
 }
